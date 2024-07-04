ace175922c11ea0061b0ceeb3df4170ef38f388a
Samir Imamov <morgingoga@gmail.com>
2024-07-04 16:59:45 +0300

    last changes

--- scripts.js
+++ scripts.js
@@ -1,24 +1,5 @@
-
-// document.getElementById('contactForm').addEventListener('submit', function(event) {
-//     event.preventDefault(); 
-
-//     const name = document.getElementById('name').value;
-//     const email = document.getElementById('email').value;
-//     const message = document.getElementById('message').value;
-
-//     if (name && email && message) {
-//         alert(`Thank you, ${name}! Your message has been sent.`);
-//     } else {
-//         alert('Please fill in all fields.');
-//     }
-// });
-
-// document.querySelectorAll('nav ul li a').forEach(anchor => {
-//     anchor.addEventListener('click', function(e) {
-//         e.preventDefault();
-
-//         document.querySelector(this.getAttribute('href')).scrollIntoView({
-//             behavior: 'smooth'
-//         });
-//     });
-// });
+function toggleMenu() {
+    var navLinks = document.getElementById("nav-links");
+    navLinks.classList.toggle("active");
+  }
+  
